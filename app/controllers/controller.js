9edf58d9ddab54593a46e3ab5856cb9091f72828
Philip McNamara <philipmcnamara1@gmail.com>
2022-03-06 18:16:56 +0000

    webPage First Draft

---
+++ app/controllers/controller.js
@@ -0,0 +1,24 @@
+const MainPage = {
+    index: {
+        handler: function (request, h) {
+            return h.file('./app/views/main.html');
+        },
+    },
+    planning: {
+        handler: function (request, h) {
+            return h.file('./app/views/planning.html');
+        },
+    },
+    data: {
+        handler: function (request, h) {
+            return h.file('./app/views/data.html');
+        },
+    },
+    about: {
+        handler: function (request, h) {
+            return h.file('./app/views/about.html');
+        },
+    },
+};
+
+module.exports = MainPage;
---
+++ routes.js
@@ -0,0 +1,17 @@
+const Controller = require('./app/controllers/controller.js');
+
+module.exports = [
+    { method: 'GET', path: '/', config: Controller.index },
+    { method: 'GET', path: '/planning', config: Controller.planning },
+    { method: 'GET', path: '/data', config: Controller.data },
+    { method: 'GET', path: '/about', config: Controller.about },
+    {
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            directory: {
+                path: './public',
+            },
+        },
+    },
+];
