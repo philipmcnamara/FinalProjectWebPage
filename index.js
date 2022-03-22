52e220fe14cf70e6524356b832b3ddd07bc39e94
Philip McNamara <philipmcnamara1@gmail.com>
2022-03-22 20:18:57 +0000

    videos embedded in

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const Hapi = require('@hapi/hapi');
 const Inert = require('@hapi/inert');
 
 const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
+    port: process.env.PORT || 3000,
 });
 
 async function init() {
